a5c75d671259d026612e0d73e27be717296c9ecd
Monikajosa <jochen@knipslicht.de>
2024-12-18 21:23:36 +0000

    Fix back button behavior and implement persistent language settings

--- admin/owner_admin_module.py
+++ admin/owner_admin_module.py
@@ -16,3 +16,16 @@ def owner_menu(update: Update, context: CallbackContext) -> None:
         update.message.reply_text(translate('owner_menu', user_lang), reply_markup=reply_markup)
     elif update.callback_query:
         update.callback_query.message.edit_text(translate('owner_menu', user_lang), reply_markup=reply_markup)
+
+def handle_owner_menu(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    query.answer()
+    user_lang = context.user_data.get('language', query.from_user.language_code)
+    if query.data == 'ad_function':
+        query.edit_message_text(text=translate('ad_function_not_implemented', user_lang))
+    elif query.data == 'module_management':
+        query.edit_message_text(text=translate('module_management_not_implemented', user_lang))
+    elif query.data == 'bot_status':
+        query.edit_message_text(text=translate('bot_status_not_implemented', user_lang))
+    elif query.data == 'back_to_main_menu':
+        start(update, context)
